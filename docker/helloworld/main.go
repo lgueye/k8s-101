7997c06b303db59862f4f0e46977ecb8ebe336f4
Peva Blanchard <peva.blanchard@gmail.com>
2020-03-24 18:59:30 +0100

    helloworld logs request to file

--- docker/helloworld/main.go
+++ docker/helloworld/main.go
@@ -4,9 +4,23 @@ import (
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
+	logfile, err := os.OpenFile("logs", os.O_APPEND | os.O_WRONLY, 0644)
+	if os.IsNotExist(err) {
+		logfile, err = os.Create("logs")
+		if err != nil {
+			panic(err)
+		}
+	} else if err != nil {
+		panic(err)
+	}
+	defer logfile.Close()
+	logfile.WriteString("======================\n")
+
 	name := "World"
 	data, err := ioutil.ReadFile("config/name")
 	if err == nil {
@@ -14,13 +28,21 @@ func main() {
 	}
 
 	r := gin.Default()
+	var count int64
+	count = 0
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "Hello " + name + "! Again!")
+		logfile.WriteString(strconv.FormatInt(count, 10) + "/ GET" + c.FullPath() + "\n")
+		count++
+		c.JSON(http.StatusOK, "Hello " + name + "!")
 	})
 	r.GET("/sickz", func (c *gin.Context) {
+		logfile.WriteString(strconv.FormatInt(count, 10) + "/ GET" + c.FullPath() + "\n")
+		count++
 		c.JSON(http.StatusInternalServerError, "I'm sick!")
 	})
 	r.GET("/healthz", func (c *gin.Context) {
+		logfile.WriteString(strconv.FormatInt(count, 10) + "/ GET" + c.FullPath() + "\n")
+		count++
 		c.JSON(http.StatusOK, "I'm healthy!")
 	})
 	_ = r.Run(":8080")
